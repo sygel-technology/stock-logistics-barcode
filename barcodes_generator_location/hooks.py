cbcc7988cbb0a908d03738fc62d68a99e549068e
sygel <alberto.martinez@sygel.es>
2025-02-12 17:01:41 +0100

    [MIG] barcodes_generator_location: Migration to 17.0

--- barcodes_generator_location/hooks.py
+++ barcodes_generator_location/hooks.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 
-def uninstall_hook(cr, registry):
+def uninstall_hook(env):
     """Delete barcode rules with generate model `stock.location` as they
     won't be useful anymore"""
-    cr.execute(
+    env.cr.execute(
         """
         DELETE FROM barcode_rule
         WHERE generate_model = 'stock.location'
